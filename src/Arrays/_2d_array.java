9e596ca75f9782cb591a40f06df629e79bf2468e
Sarthak M. Wagh <sarthakwagh16@gmail.com>
2022-12-15 16:59:17 +0530

    2D array intro

---
+++ src/Arrays/_2d_array.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class _2d_array {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter row size:");
+        int row=sc.nextInt();
+        System.out.println("enter col size:");
+        int col=sc.nextInt();
+
+        // declare 2D array
+        int[][] stud=new int[row][col];
+        System.out.println("enter array items:");
+        // take input
+        for (int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                stud[i][j]=sc.nextInt();
+            }
+        }
+
+        // output 2D array
+        System.out.println("array items are:");
+        for (int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(stud[i][j]+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
